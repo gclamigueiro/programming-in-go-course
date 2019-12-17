d4bfcba215c841158be6ffd610c136f082d018db
Gabriel Camps <gabriel9891cl@gmail.com>
2019-12-17 14:40:43 -0500

    add hello.go file

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main // package main is a special package, it tells that it's an executable program
+
+import "fmt" // format package, contains functions for formatting output
+
+// entry point for this program,
+func main() { // important: curly brace has to be on the same line as your function declaration
+
+	fmt.Println("Hello World") // print Hello World
+}
