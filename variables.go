6611558cfd3b0ea74e3f03404a8407846c94778e
Gabriel Camps <gabriel9891cl@gmail.com>
2019-12-17 18:20:04 -0500

    variables

---
+++ variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// variables could be declares in package scope
+// using var variableName dataType
+// or passing a value and the compiler infer the type
+var hello = "Hello World"
+
+// you can declare multiple variables at once.
+var a1, a2 int
+
+//Variables declared without a corresponding initialization are zero-valued.
+//For example, the zero value for an int is 0. false for bool and "" for string
+
+func main() {
+
+	// you can use de := shorthand for declaring and initializing variables
+	// but only in function scope
+	bye := "Bye World"
+
+	a1 = 1
+	a2 = 2
+
+	// declaring a constant value
+	const notChange bool = true
+
+	fmt.Println(a1, hello)
+	fmt.Println(a2, bye)
+}
